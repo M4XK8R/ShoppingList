5fe2d066ba167eb5606151f37824c9050c6b24f0
M4XK8R <kpopmyhope@gmail.com>
2023-06-27 18:02:00 +0300

    added ShopListRepositoryImpl and some changes

--- app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
@@ -2,7 +2,8 @@ package com.example.shoppinglist.domain
 
 interface ShopListRepository {
     fun addShopItem(shopItem: ShopItem)
-    fun deleteShopItem(id: Int)
+    fun deleteShopItem(shopItem: ShopItem)
+
     fun editShopItem(shopItem: ShopItem)
     fun getShopItemById(shopItemId: Int): ShopItem
     fun getShopList(): List<ShopItem>
