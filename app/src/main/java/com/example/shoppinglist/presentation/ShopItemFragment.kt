0eced22a627c97fcc4f430bfa92ae39c27c2328e
M4XK8R <kpopmyhope@gmail.com>
2023-07-02 15:29:49 +0300

    ShopItemFragment is created

---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemFragment.kt
@@ -0,0 +1,161 @@
+package com.example.shoppinglist.presentation
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class ShopItemFragment : Fragment() {
+
+    private lateinit var viewModel: ShopItemViewModel
+    private lateinit var editTextName: TextInputEditText
+    private lateinit var editTextCount: TextInputEditText
+    private lateinit var tilName: TextInputLayout
+    private lateinit var tilCount: TextInputLayout
+    private lateinit var buttonSave: Button
+
+    private var screenMode: String = MODE_UNKNOWN
+    private var shopItemId: Int = ShopItem.UNDEFINED_ID
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseParams()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_shop_item, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        initViews(view)
+        addTextChangeListeners()
+        setUpScreenMode()
+        observeViewModel()
+    }
+
+
+    /**
+    Private functions
+     */
+    private fun observeViewModel() {
+        viewModel.isInputCountInvalidLd.observe(viewLifecycleOwner) {
+            val errorMessage = if (it) getString(R.string.error_input_count) else null
+            tilCount.error = errorMessage
+        }
+        viewModel.isInputNameInvalidLd.observe(viewLifecycleOwner) {
+            val errorMessage = if (it) getString(R.string.error_input_name) else null
+            tilName.error = errorMessage
+        }
+        viewModel.isScreenCanBeClosedLd.observe(viewLifecycleOwner) {
+//           activity?.onBackPressed()
+            activity?.onBackPressedDispatcher?.onBackPressed()
+        }
+    }
+
+    private fun setUpScreenMode() {
+        when (screenMode) {
+            MODE_EDIT -> launchEditMode()
+            MODE_ADD -> launchAddMode()
+        }
+    }
+
+    private fun addTextChangeListeners() {
+        editTextName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetErrorInputName()
+            }
+        })
+        editTextCount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetErrorInputCount()
+            }
+        })
+    }
+
+    private fun launchEditMode() {
+        viewModel.getShopItemById(shopItemId)
+        viewModel.shopItemLd.observe(this) {
+            editTextName.setText(it.name)
+            editTextCount.setText(it.count.toString())
+        }
+        buttonSave.setOnClickListener {
+            viewModel.editShopItem(editTextName.text?.toString(), editTextCount.text?.toString())
+        }
+    }
+
+    private fun launchAddMode() {
+        buttonSave.setOnClickListener {
+            viewModel.addShopItem(editTextName.text?.toString(), editTextCount.text?.toString())
+        }
+    }
+
+    private fun parseParams() {
+        val args = requireArguments()
+        if (!args.containsKey(SCREEN_MODE)) {
+            throw RuntimeException("Param screen mode is absent")
+        }
+        val mode = args.getString(SCREEN_MODE)
+        if (mode != MODE_EDIT && mode != MODE_ADD) {
+            throw RuntimeException("Unknown screen mode $mode")
+        }
+        screenMode = mode
+        if (screenMode == MODE_EDIT) {
+            if (!args.containsKey(SHOP_ITEM_ID)) {
+                throw RuntimeException("Param shop item id is absent")
+            }
+            shopItemId = args.getInt(SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+        }
+    }
+
+    private fun initViews(view: View) {
+        editTextName = view.findViewById(R.id.etName)
+        editTextCount = view.findViewById(R.id.etCount)
+        tilName = view.findViewById(R.id.tilName)
+        tilCount = view.findViewById(R.id.tilCount)
+        buttonSave = view.findViewById(R.id.saveButton)
+    }
+
+    companion object {
+        private const val SCREEN_MODE = "screen_mode"
+        private const val SHOP_ITEM_ID = "shop_item_id"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = ""
+
+        fun newInstanceAddItem(): ShopItemFragment {
+            return ShopItemFragment().apply {
+                arguments = Bundle().apply {
+                    putString(SCREEN_MODE, MODE_ADD)
+                }
+            }
+        }
+
+        fun newInstanceEditItem(shopItemId: Int): ShopItemFragment {
+            return ShopItemFragment().apply {
+                arguments = Bundle().apply {
+                    putString(SCREEN_MODE, MODE_EDIT)
+                    putInt(SHOP_ITEM_ID, shopItemId)
+                }
+            }
+        }
+    }
+}
